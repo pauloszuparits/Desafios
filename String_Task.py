6f129b1d9d78d9092989a40cc1e7465ba629042f
Paulo Szuparits <pauloszuparits@hotmail.com>
2023-03-05 18:17:08 -0300

    Desafio String_Task feito

---
+++ String_Task.py
@@ -0,0 +1,13 @@
+string = input().upper()
+resp = []
+for letter in string:
+    if letter == "A" or letter == "O" or letter == "Y" or letter == "E" or letter == "U" or letter == "I":
+        continue;
+    else:
+        resp.append(".")
+        resp.append(letter)
+    
+        
+resp2 = "".join(resp)
+print(resp2.lower())
+    
